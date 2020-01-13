cf240bb04b302f083b9d48839880e774ef7ce7f7
estela19 <e9857256@naver.com>
2020-01-13 19:19:14 +0900

    Feat(post) : Add post method

--- simpleblog/blog/models.py
+++ simpleblog/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
     objects = models.Manager()
@@ -11,3 +12,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
--- simpleblog/blog/views.py
+++ simpleblog/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
 
 def post_list(request):
     posts=Post.objects.filter(published_date__isnull=False).order_by('-created_date')
@@ -15,3 +17,21 @@ def post_detail(request, pk):
         'post' : post,
     }
     return render(request, 'blog/post_detail.html', context)
+
+def post_add(request):
+    if request.method == 'POST':
+        User = get_user_model()
+        author = User.objects.get(username='admin')
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post.objects.create(
+            author = author,
+            title = title,
+            content = content,
+        )
+        post.publish()
+        post_pk = post.pk
+        return redirect(post_detail, pk=post_pk) 
+
+    elif request.method == "GET":
+        return render(request, 'blog/post_add.html')
